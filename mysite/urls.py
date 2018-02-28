58fda7cc5fe6ee18398c4c41fc4bb1fc6e462696
HalukaMB <mail@maier-borst.de>
2018-03-01 00:20:14 +0100

    base url

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.urls import include, path
 from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
-    path('honoradar/', include('honoradar.urls')),
     path('admin/', admin.site.urls),
-   
+    url(r'^',include('honoradar.urls'))
+
 ]
