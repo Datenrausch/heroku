e5bc789caaa4026cae6813a6988446b621ca82b5
HalukaMB <mail@maier-borst.de>
2018-04-12 22:32:55 +0200

    changed root url for static

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
+STATIC_URL = '../honoradar/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
