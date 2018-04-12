c57cdbcb6e4e665f2844cc855ad6e3af20f5d18b
HalukaMB <mail@maier-borst.de>
2018-04-12 22:21:54 +0200

    changing static root in setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/honoradar/static/')
 STATIC_URL = '/honoradar/static/'
 
 # Extra places for collectstatic to find static files.
