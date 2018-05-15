1761478d44b1c88821b25677ae55a6e314746f30
HalukaMB <mail@maier-borst.de>
2018-05-15 22:24:35 +0200

    changing models

--- honoradar/views.py
+++ honoradar/views.py
@@ -1414,29 +1414,6 @@ class IndexView(generic.ListView):
 
 
 
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'honoradar/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'honoradar/results.html'
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'honoradar/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
