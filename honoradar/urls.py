3dbd7c2fe887f8aa8152b332c32d22610acfbe59
HalukaMB <mail@maier-borst.de>
2018-05-15 22:27:33 +0200

    fixed urls.p<

--- honoradar/urls.py
+++ honoradar/urls.py
@@ -6,9 +6,6 @@ from . import views
 app_name = 'honoradar'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('senddata/', views.senddata, name='senddata'),
     path('getdata/', views.getdata, name='getdata'),
 ]
