72abfa6ec7fbed41186b5b8352ff54f6c08e3f3a
HalukaMB <mail@maier-borst.de>
2018-05-26 18:15:23 +0200

    changed password

--- honoradar/admin.py
+++ honoradar/admin.py
@@ -19,28 +19,10 @@ class MediumAdmin(admin.ModelAdmin):
 ]
     inlines = [DataCollection]
     #this is for the question page
-    list_display = ['mediumname','freeoremployed']
+    list_display = ['mediumname','freeoremployed','UpDate']
     #fields = ['pub_date', 'question_text']
-    list_filter = ['freeoremployed']
+    list_filter = ['freeoremployed','UpDate']
     search_fields = ['mediumname']
-
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
-
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-    #this is for the question page
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    #fields = ['pub_date', 'question_text']
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
 
 
 admin.site.register(Medium, MediumAdmin)
-admin.site.register(Question, QuestionAdmin)
