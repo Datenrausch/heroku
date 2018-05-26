346319e0e86e23360571a22828b4fbdbe3954bd8
HalukaMB <mail@maier-borst.de>
2018-05-26 20:01:42 +0200

    change admin page

--- honoradar/admin.py
+++ honoradar/admin.py
@@ -15,11 +15,12 @@ class MediumAdmin(admin.ModelAdmin):
     fieldsets = [
     (None,               {'fields': ['mediumname'] }),
     ('Jobstatus', {'fields': ['freeoremployed']}),
+    ('Fairness', {'fields': ['fairness']})
 
 ]
     inlines = [DataCollection]
     #this is for the question page
-    list_display = ['mediumname','freeoremployed','UpDate']
+    list_display = ['mediumname','freeoremployed','UpDate','fairness']
     #fields = ['pub_date', 'question_text']
     list_filter = ['freeoremployed','UpDate']
     search_fields = ['mediumname']
