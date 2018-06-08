99673eabaf1f54d3d7a37d2ca1693ef5f850e1d6
HalukaMB <mail@maier-borst.de>
2018-06-08 22:54:17 +0200

    extraline of whitenoise

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
