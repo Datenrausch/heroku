a7e26922c8ae63e6e8b6a78366d4b25fa8aa0afd
HalukaMB <mail@maier-borst.de>
2018-06-14 23:38:07 +0200

    here we go again

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,9 +128,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
