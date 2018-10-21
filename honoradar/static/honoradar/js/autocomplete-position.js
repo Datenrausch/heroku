bb9fa84859b45a30bd0794905f823a3991d83998
HalukaMB <mail@maier-borst.de>
2018-10-21 12:51:34 +0200

    michel fixes part 1

--- honoradar/static/honoradar/js/autocomplete-position.js
+++ honoradar/static/honoradar/js/autocomplete-position.js
@@ -1,22 +1,22 @@
 var options = {
-  url: "static/honoradar/position.json",
+    url: "static/honoradar/position.json",
 
-getValue: function(element) {
-return element.name;
-},
-requestDelay: 1000,
-list: {
-maxNumberOfElements: 5,
-match: {
-  enabled: true
-},
+    getValue: function(element) {
+        return element.name;
+    },
+    requestDelay: 1000,
+    list: {
+        maxNumberOfElements: 5,
+        match: {
+            enabled: true
+        },
 
-onSelectItemEvent: function() {
-var value = $("#data_position").getSelectedItemData().code;
+        onSelectItemEvent: function() {
+            var value = $("#data_position").getSelectedItemData().code;
 
-$("#data_position").val(value).trigger("change");
-}
-}
+            $("#data_position").val(value).trigger("change");
+        }
+    }
 };
 
 $("#data_position").easyAutocomplete(options);
