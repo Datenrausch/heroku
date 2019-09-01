c04a2b2b2f55d3732db403f772210f9b1e1c70af
HalukaMB <mail@maier-borst.de>
2019-09-01 14:38:23 +0200

    update autocomplete 1

---
+++ honoradar/static/honoradar/js/js/autocomplete-giv.js
@@ -0,0 +1,25 @@
+//Only checking for the names in the inputs
+function autocomplete_giv() {
+var versionUpdate = (new Date()).getTime();
+var options = {
+  data: nameJSON["autofilljson"],
+
+    getValue: function(element) {
+        return element.name;
+    },
+    requestDelay: 1000,
+    list: {
+        maxNumberOfElements: 5,
+        match: {
+            enabled: true
+        },
+
+        onChooseEvent: function() {
+            var value = $("#data_medium").getSelectedItemData().code;
+
+            $("#data_medium").val(value).trigger("change");
+        }
+    }
+};
+
+$("#data_medium").easyAutocomplete(options);}
